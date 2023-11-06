ea9e09404ac3f786e13f5a34fa43b3a16ed0a653
PHoaiLe <lehoaiphuong13042002@gmail.com>
2023-11-06 17:36:37 +0700

    feat: top nav bar

---
+++ src/app/auth/sign_up/layout.js
@@ -0,0 +1,13 @@
+
+function AuthRootLayout({children})
+{
+    return(
+        <html lang='en'>
+            <body>
+                {children}
+            </body>
+        </html>
+    )
+}
+
+export default AuthRootLayout
