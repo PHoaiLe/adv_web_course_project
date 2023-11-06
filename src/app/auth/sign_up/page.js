23c11b62ae25b3ef61482804b7a83139c4b5ff48
PHoaiLe <lehoaiphuong13042002@gmail.com>
2023-11-06 21:49:23 +0700

    chore: add back button

--- src/app/auth/sign_up/page.js
+++ src/app/auth/sign_up/page.js
@@ -3,6 +3,7 @@ import Link from 'next/link';
 
 function Page()
 {
+    let registerBackButton = 'register_back_button'
     let usernameKey = 'register_username_key'
     let fullnameKey = 'register_fullname_key'
     let emailKey = 'register_email_key';
@@ -11,40 +12,46 @@ function Page()
     let registerButtonKey = 'register_button_key'
 
     return(
-        <div className='register-frame'>
-            <div className='register-form-frame'>
-                <div className='register-form-wrapper'>
-                    <div className='register-retangle'>
-                        <p>Sign up</p>
-                        <Link className='register-to-login-link' href='/auth/sign_in'>I had an account</Link>
-                    </div>
+        <>
+            <div className='register-back-homepage'>
+                <Link key={registerBackButton} href='/'>Back</Link>
+            </div>
+            <div className='register-frame'>
+                <div className='register-form-frame'>
+                    <div className='register-form-wrapper'>
+                        <div className='register-retangle'>
+                            <p>Sign up</p>
+                            <Link className='register-to-login-link' href='/auth/sign_in'>I had an account</Link>
+                        </div>
 
-                    <div className='register-form'>
-                        <div>
-                            <form>
-                                <label>Fullname</label> <br/>
-                                <input key={fullnameKey} type='text'/> <br/>
-                                <label>Username</label> <br/>
-                                <input key={usernameKey} type='text'/> <br/>
-                                <label>Email</label> <br/>
-                                <input key={emailKey} type='text'/> <br/>
-                                <label>Password</label> <br/>
-                                <input key={password} type='password'/> <br/>
-                                <label>Confirm Password</label> <br/>
-                                <input key={confirmPassword} type='password'/>
-                            </form>
-                            <br/>
-                            <button key={registerButtonKey}>Sign up</button>
-                            <br/>
+                        <div className='register-form'>
                             <div>
+                                <form>
+                                    <label>Fullname</label> <br/>
+                                    <input key={fullnameKey} type='text'/> <br/>
+                                    <label>Username</label> <br/>
+                                    <input key={usernameKey} type='text'/> <br/>
+                                    <label>Email</label> <br/>
+                                    <input key={emailKey} type='text'/> <br/>
+                                    <label>Password</label> <br/>
+                                    <input key={password} type='password'/> <br/>
+                                    <label>Confirm Password</label> <br/>
+                                    <input key={confirmPassword} type='password'/>
+                                </form>
+                                <br/>
+                                <button key={registerButtonKey}>Sign up</button>
+                                <br/>
+                                <div>
 
+                                </div>
                             </div>
                         </div>
+                        
                     </div>
-                    
                 </div>
             </div>
-        </div>
+        </>
+
     )
 }
 
