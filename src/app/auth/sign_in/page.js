e08fd5804b7d7af422c0b1379356101e7c0da061
PHoaiLe <lehoaiphuong13042002@gmail.com>
2023-11-06 21:49:23 +0700

    chore: add back button

--- src/app/auth/sign_in/page.js
+++ src/app/auth/sign_in/page.js
@@ -4,19 +4,24 @@ import Link from 'next/link';
 
 function Page()
 {
+    let backButton = 'login_back_homepage_button'
     let emailKey = 'login_email_key';
     let password = 'login_password_key'
     let loginButtonKey = 'login_button_key'
 
     return(
-        <div className='login-frame'>
+        <>
+            <div className='login-back-homepage'>
+                <Link key={backButton} href='/'>Back</Link>
+            </div>
+            <div className='login-frame'>
             <div className='login-form-frame'>
                 <div className='login-form-wrapper'>
                     <div className='login-form'>
                         <div>
                             <form>
-                                <label>Email</label> <br/>
-                                <input key={emailKey} type='text'/> <br/>
+                                <label id='login-email-label'>Email</label> <br/>
+                                <input key={emailKey} type='text' label='login-email-label'/> <br/>
                                 <label>Password</label> <br/>
                                 <input key={password} type='password'/>
                             </form>
@@ -35,7 +40,9 @@ function Page()
                     </div>
                 </div>
             </div>
-        </div>
+            </div>
+        </>
+
     )
 }
 
