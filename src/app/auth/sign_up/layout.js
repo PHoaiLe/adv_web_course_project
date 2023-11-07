22ac688f3acba7025b4e25f498c53710137c3e2d
Chamelsqr999 <lequockyanh99@gmail.com>
2023-11-07 21:35:35 +0700

    add features

---
+++ src/app/auth/sign_up/layout.js
@@ -0,0 +1,13 @@
+
+function RootLayout({children})
+{
+    return(
+        <html lang='en'>
+            <body>
+                {children}
+            </body>
+        </html>
+    )
+}
+
+export default RootLayout
