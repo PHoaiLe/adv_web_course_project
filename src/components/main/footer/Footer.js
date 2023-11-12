74fad618da4dc726a3d7cef8dc76ebd754253a9a
Chamelsqr999 <lequockyanh99@gmail.com>
2023-11-13 01:11:48 +0700

    update footer, landing page

--- src/components/main/footer/Footer.js
+++ src/components/main/footer/Footer.js
@@ -1,48 +1,60 @@
 export default function Footer(){
     return(
         <>
-            <footer className="text-gray-600 body-font">
-  <div className="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
-    <div className="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
-      <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-        <span className="ml-3 text-xl">University of Science</span>
-      </a>
-    </div>
-    <div className="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
-      <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-        <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">Web Advanced Progamming-20KTPM2</h2>
-      </div>
-      <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-        <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">Members</h2>
-        <nav className="list-none mb-10">
-          <li>
-            <a className="text-gray-600 hover:text-gray-800">Member1</a>
-          </li>
-          <li>
-            <a className="text-gray-600 hover:text-gray-800">Member2</a>
-          </li>
-          <li>
-            <a className="text-gray-600 hover:text-gray-800">Member3</a>
-          </li>
-        </nav>
-      </div>
-      <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-        <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">Email</h2>
-        <nav className="list-none mb-10">
-          <li>
-            <a className="text-gray-600 hover:text-gray-800">Email1</a>
-          </li>
-          <li>
-            <a className="text-gray-600 hover:text-gray-800">Email2</a>
-          </li>
-          <li>
-            <a className="text-gray-600 hover:text-gray-800">Email3</a>
-          </li>
-        </nav>
-      </div>
-    </div>
-  </div>
-</footer>
+            <footer className="p-4 bg-gray-50 sm:p-6 dark:bg-dark-gray-800">
+        <div className="mx-auto max-w-screen-xl">
+            <div className="md:flex md:justify-between">
+                <div className="mb-6 md:mb-0">
+                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Web Advanced Programming</span>
+                </div>
+                <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+                    <div>
+                        <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Members</h2>
+                        <ul className="text-gray-600 dark:text-gray-400">
+                            <li>
+                                <p>Member1</p>
+                            </li>
+                            <li>
+                                <p>Member2</p>
+                            </li>
+                            <li>
+                                <p>Member3</p>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
+                        <ul className="text-gray-600 dark:text-gray-400">
+                            <li className="mb-4">
+                                <a href="#" className="hover:underline ">Github</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Email</h2>
+                        <ul className="text-gray-600 dark:text-gray-400">
+                            <li>
+                                <p>Member1</p>
+                            </li>
+                            <li>
+                                <p>Member2</p>
+                            </li>
+                            <li>
+                                <p>Member3</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+            <div className="sm:flex sm:items-center sm:justify-between">
+                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">University of Science</span>
+                <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
+                  <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">227 Nguyen Van Cu, District 5, Ho Chi Minh City</span>
+                </div>
+            </div>
+        </div>
+    </footer>
         </>
     )
 }
