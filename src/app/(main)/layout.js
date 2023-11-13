4b702f649174a09bbbcfeec138457171516c6957
PHoaiLe <lehoaiphuong13042002@gmail.com>
2023-11-13 10:10:48 +0700

    fix: not found module svg, tailwinds css

--- src/app/(main)/layout.js
+++ src/app/(main)/layout.js
@@ -1,5 +1,5 @@
 import { Inter } from 'next/font/google'
-import './globals.css'
+import '../globals.css'
 import WebTopNavBar from '@/components/main/top_nav_bar/TopNavBar'
 import Footer from '@/components/main/footer/Footer'
 
