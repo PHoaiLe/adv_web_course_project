207d7bc03dba9c9faefe4965c87f1ecb477f4cec
Chamelsqr999 <lequockyanh99@gmail.com>
2023-11-13 16:17:01 +0700

    add dashboard

---
+++ src/app/(dashboard)/dashboard/layout.js
@@ -0,0 +1,35 @@
+import Navbar from "@/components/Navbar.js";
+import Sidebar from "@/components/Sidebar.js";
+
+function DashboardRootLayout({children})
+{
+    return(
+        <html lang='en'>
+            <body>
+            <Sidebar/>
+            <Navbar/>
+                {children}
+            </body>
+        </html>
+    )
+}
+export default DashboardRootLayout
+
+// 'use client';
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+// import "@fortawesome/fontawesome-free/css/all.min.css";
+
+// import Dashboard from "@/view/dashboard";
+
+// ReactDOM.render(
+//     <BrowserRouter>
+//       <Switch>
+//         <Route path="/dashboard" component={Dashboard} />
+//         <Redirect from="/" to="/dashboard" />
+//       </Switch>
+//     </BrowserRouter>,
+//     document.getElementById("root")
+//   );
